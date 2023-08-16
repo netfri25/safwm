b6f6ce96544c1fe3bc616e94c2100b09ff51cf81
Netanel Friedman <netanel.friedman6@gmail.com>
2023-08-16 21:57:14 +0300

    Fix borders being opaque

--- config.def.h
+++ config.def.h
@@ -7,9 +7,9 @@
 #define SCREEN_WIDTH 1600
 #define SCREEN_HEIGHT 900
 
-#define BORDER_NORMAL 0x7a8478
-#define BORDER_FOCUS  0xd3c6aa
-#define BORDER_NONE   0x000000
+#define BORDER_NORMAL 0xff7a8478
+#define BORDER_FOCUS  0xffd3c6aa
+#define BORDER_NONE   0x00000000
 #define BORDER_WIDTH  3
 #define WINDOW_GAP    10
 
